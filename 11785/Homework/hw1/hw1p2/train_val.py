f369d350de22be17c53079fb5b46fbbd9a90a13d
Stevenhyh1 <yihe_hua@outlook.com>
2020-02-09 15:27:08 -0500

    model.py

--- 11785/Homework/hw1/hw1p2/train_val.py
+++ 11785/Homework/hw1/hw1p2/train_val.py
@@ -6,20 +6,23 @@ import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader, Dataset
+import faulthandler
+
+faulthandler.enable()
 
 import utils
 from model import MLP
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size',type=int, default=512, help='Batch size')
-parser.add_argument('--lr',type=float,default=0.001,help='Learning rate')
+parser.add_argument('--batch_size',type=int, default=256, help='Batch size')
+parser.add_argument('--lr',type=float,default=0.0002,help='Learning rate')
 parser.add_argument('--momentum',type=float,default=0.9,help='SGD momentum')
 parser.add_argument('--num_epoch',type=float,default=15,help='Number of Epoch')
 parser.add_argument('--data_path',type=str,default='/home/yihe/Data')
 parser.add_argument('--label_path',type=str,default='/home/yihe/Data')
 parser.add_argument('--save_file',type=str,default='./model.pth.tar')
-parser.add_argument('--k', type=int, default=12)
+parser.add_argument('--k', type=int, default=16)
 args = parser.parse_args()
 
 def Train(data, model, criterion, optimizer):
